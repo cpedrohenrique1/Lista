f85e033f82fc8920bb820cbea8f012fa20186ed7
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-03-17 08:43:47 -0300

    Add files via upload

---
+++ Lista/no.cpp
@@ -0,0 +1,37 @@
+#include "no.h"
+
+Pedro::NO::NO():
+    dado(0),
+    proximo(0)
+{
+
+}
+
+Pedro::NO::NO(int elemento):
+    dado(elemento),
+    proximo(0)
+{
+    
+}
+
+int Pedro::NO::getDado() const
+{
+    return dado;
+}
+
+void Pedro::NO::setDado(int newDado)
+{
+    dado = newDado;
+}
+
+Pedro::NO *Pedro::NO::getProximo() const
+{
+    return proximo;
+}
+
+void Pedro::NO::setProximo(NO *newProximo)
+{
+    proximo = newProximo;
+}
+
+
---
+++ Lista/no.h
@@ -0,0 +1,21 @@
+#ifndef NO_H
+#define NO_H
+
+namespace Pedro{
+    class NO
+    {
+    private:
+        int dado;
+        NO *proximo;
+    public:
+        NO();
+        NO(int elemento);
+        int getDado() const;
+        void setDado(int newDado);
+        NO *getProximo() const;
+        void setProximo(NO *newProximo);
+    };
+}
+
+
+#endif // NO_H
