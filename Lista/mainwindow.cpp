cc6819ef57662ca8a9cd20d9eb5378030c0cbc3a
Pedro Henrique <phpedrohenrique474@gmail.com>
2023-03-21 20:43:35 -0300

    update

--- Lista/mainwindow.cpp
+++ Lista/mainwindow.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <ctime>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,14 +13,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_botaoTestar_clicked()
 {
-    try {
+    try
+    {
+        srand(time(0));
         Pedro::LLSE lista;
-
-    } catch (...) {
+        for (int i = 0; i < 5000; i++)
+        {
+            lista.inserirInicio(rand() % 101);
+        }
+        QString saida = "";
+        for (int i = 0; i < 5001; i++)
+        {
+            saida += QString::number(lista.retirarInicio()) + "\t";
+        }
+        ui->saida->setText(saida);
+    }
+    catch (...)
+    {
         QMessageBox::critical(this, "ERRO ", "erro de teste");
     }
 }
-
